61ed974a3a6bacf2b1204d05d7c66048f65e48be
FS <fstriewski@outlook.com>
2020-12-11 19:34:33 +0100

    Fix investigator code

--- src/app.ts
+++ src/app.ts
@@ -11,18 +11,22 @@ app.use(bodyParser.json())
 
 app.get(`/bydate/:icode`, async (req, res) => {
   const { icode } = req.params
-  const queryResult = await prisma.decks.findMany({
+  console.log(icode[0])
+  const investigatorCode = icode[0] === '0' ? icode.slice(1,5) : icode // Leading 0 needs to go
+  console.log(investigatorCode)
+  return await prisma.decks.findMany({
     where: {
-      investigator_code: String(icode),
+      investigator_code: String(investigatorCode),
     },
           select:{
             investigator_code: true,
             date: true,
           },
-  })
-  const hist = generateHistogram(queryResult, SCALE.MONTH)
-  console.log(hist)
-  res.json(hist)
+  }).then(queryResult => {
+    const hist = generateHistogram(queryResult, SCALE.MONTH)
+    // console.log(hist)
+    return res.json(hist)
+  }).catch(e => console.log(e))
 })
 
 app.get('/', (req, res) => res.send('Hello World!'));
